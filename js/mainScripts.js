21c9d09d172168530ea809c00cbce87dfc32d22f
Mattias Stjernström <mk.stjernstrom@gmail.com>
2024-01-17 20:35:09 +0100

    Innit commit

---
+++ js/mainScripts.js
@@ -0,0 +1,12 @@
+window.onscroll = function () {
+  progressBar();
+};
+
+function progressBar() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("progress-bar").style.width = scrolled + "%";
+}
